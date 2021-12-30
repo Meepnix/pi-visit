e7dba40029d14578085169125906506ecb2d157a
Chris Massey <moonix.uk@gmail.com>
2021-12-30 20:11:58 +0000

    sound

--- pi_visit/scan/scan.py
+++ pi_visit/scan/scan.py
@@ -1,11 +1,14 @@
 import cv2
+import os
 from pi_visit.database import database
 
 
 class Scan():
 
     def __init__(self):
-
+        self.in_message = os.environ['IN_MESSAGE']
+        self.out_message = os.environ['OUT_MESSAGE']
+        self.directory = os.environ['IMG_DIRECTORY']
         # QR code detection object
         self.detector = cv2.QRCodeDetector()
         self.dbo = database.database()
@@ -16,67 +19,101 @@ class Scan():
     def scan_qr_img(self, img):
 
         # get bounding box coords and data
-        retval, decoded_info, points, straight_qrcode = self.detector.detectAndDecodeMulti(
+        retval, decoded_info, vectors, straight_qrcode = self.detector.detectAndDecodeMulti(
             img)
 
         if len(decoded_info) == 2:
             print("decoded_info[0]", decoded_info[0])
             print("decoded_info[1]", decoded_info[1])
 
-            if decoded_info[0] or decoded_info[1]:
+            if decoded_info[0] and decoded_info[1]:
                 print("data found: ", decoded_info)
 
+                self.success_qr, self.status = self.dbo.log_qr(decoded_info[0])
+
+                return self.process_img(vectors, img, decoded_info[0])
+
+
+                """
+                
                 # database log
                 if decoded_info[0]:
                     self.success_qr, self.status = self.dbo.log_qr(decoded_info[0])
 
+                    return self.process_img(vectors, img, decoded_info[0])
+
                 elif decoded_info[1]:
                     self.success_qr, self.status = self.dbo.log_qr(decoded_info[1])
 
-                # Scan img
+                    return self.process_img(vectors, img, decoded_info[1])
+                """
 
-                first_array_one = points[0][0].astype(int).tolist()
-                first_array_two = points[0][1].astype(int).tolist()
-                first_array_third = points[0][2].astype(int).tolist()
 
-                second_array_one = points[1][0].astype(int).tolist()
-                second_array_two = points[1][1].astype(int).tolist()
-                second_array_third = points[1][2].astype(int).tolist()
+    def process_img(self, points, image, filename):
 
-                # test which QR points is left or right
-                if first_array_two[0] > second_array_two[0]:
-                    x = second_array_two[0]
-                    y = second_array_two[1]
-                    w = first_array_one[0] - x
-                    h = first_array_third[1] - y
-                else:
-                    x = first_array_two[0]
-                    y = first_array_two[1]
-                    w = second_array_one[0] - x
-                    h = second_array_third[1] - y
+        self.cropped_img = self.crop_img(points, image)
 
-                self.crop_img = img[y:y+h, x:x+w]
+        if len(self.cropped_img) > 0:
+            self.success_img = True
+            self.save_img(filename)
 
-                if len(self.crop_img) > 0:
-                    self.success_img = True
+            return True
 
-                    return True
+
+    def crop_img(self, points, image):
+        # Scan img
+
+        first_array_one = points[0][0].astype(int).tolist()
+        first_array_two = points[0][1].astype(int).tolist()
+        first_array_third = points[0][2].astype(int).tolist()
+
+        second_array_one = points[1][0].astype(int).tolist()
+        second_array_two = points[1][1].astype(int).tolist()
+        second_array_third = points[1][2].astype(int).tolist()
+
+        # test which QR points is left or right
+        if first_array_two[0] > second_array_two[0]:
+            x = second_array_two[0]
+            y = second_array_two[1]
+            w = first_array_one[0] - x
+            h = first_array_third[1] - y
+        else:
+            x = first_array_two[0]
+            y = first_array_two[1]
+            w = second_array_one[0] - x
+            h = second_array_third[1] - y
+        
+        return image[y:y+h, x:x+w]
     
     def get_img(self):
 
         if self.success_img:
-            return self.crop_img
+            return self.cropped_img
         else:
             return False
     
     def get_status(self):
+
         if self.success_qr:
-            return self.status
+            if self.status == 'IN':
+                return self.in_message
+            else:
+                return self.out_message
         else:
             return "ERROR"
 
-    def reset(self):
-        pass
+    def reset_success(self):
+
+        self.status = ''
+        self.success_img = False
+        self.success_qr = False
+
+    def save_img(self, filename):
+
+        os.chdir(self.directory)
+        cv2.imwrite(filename + '.jpg', self.cropped_img)
+
+
 
 
 
--- pi_visit/ui/ui.py
+++ pi_visit/ui/ui.py
@@ -1,36 +1,42 @@
 from PySide2.QtCore import QSize, QTimer
-from PySide2.QtGui import QImage, QPixmap
+from PySide2.QtGui import QImage, QPixmap, QFont
 from PySide2.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout
+
+from pydub import AudioSegment
+from pydub.playback import play
+
 from pi_visit.scan import scan
 import cv2
 import sys
 
+import pygame
+
+
+
 class MainApp(QWidget):
 
     def __init__(self):
+
         QWidget.__init__(self)
         self.video_size = QSize(960, 540)
         self.setup_ui()
         self.setup_camera()
-
+        self.setup_sound()
         self.scan = scan.Scan()
+        self.frame = ""
 
     def setup_ui(self):
         
         self.image_label = QLabel()
         self.image_label.setFixedSize(self.video_size)
-
         self.status_label = QLabel()
-
-
+        self.status_label.setFont(QFont('Arial', 30))
         self.quit_button = QPushButton("Quit")
         self.quit_button.clicked.connect(self.close)
-
         self.main_layout = QVBoxLayout()
         self.main_layout.addWidget(self.image_label)
         self.main_layout.addWidget(self.status_label)
         self.main_layout.addWidget(self.quit_button)
-
         self.setLayout(self.main_layout)
 
     def setup_camera(self):
@@ -38,27 +44,27 @@ class MainApp(QWidget):
         self.capture = cv2.VideoCapture(0)
         self.capture.set(cv2.CAP_PROP_FRAME_WIDTH, self.video_size.width())
         self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT, self.video_size.height())
-
         self.timer = QTimer()
         self.timer.timeout.connect(self.display_video_stream)
         self.timer.start(30)
 
     def display_video_stream(self):
         
-        _, frame = self.capture.read()
-
-        image_result = self.scan.scan_qr_img(frame)
+        _, self.frame = self.capture.read()
+        image_result = self.scan.scan_qr_img(self.frame)
 
         if image_result:
+            self.play_success_sound()
             self.timer.stop()
-            frame = self.scan.get_img()
+            self.frame = self.scan.get_img()
             self.check_show_status()
-            QTimer.singleShot(5000, self.continue_scan)
+            QTimer.singleShot(15000, self.continue_scan)
+
             
-        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+        self.frame = cv2.cvtColor(self.frame, cv2.COLOR_RGB2BGR)
         #frame = cv2.flip(frame, 1)
-        image = QImage(frame, frame.shape[1], frame.shape[0], 
-                       frame.strides[0], QImage.Format_RGB888)
+        image = QImage(self.frame, self.frame.shape[1], self.frame.shape[0], 
+                       self.frame.strides[0], QImage.Format_RGB888)
         self.image_label.setPixmap(QPixmap.fromImage(image))
 
     def check_show_status(self):
@@ -68,7 +74,31 @@ class MainApp(QWidget):
     
     def continue_scan(self):
 
+        self.capture.release()
+        self.capture = cv2.VideoCapture(0)
+        self.capture.set(cv2.CAP_PROP_FRAME_WIDTH, self.video_size.width())
+        self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT, self.video_size.height())
         self.timer.start(30)
+
+    def setup_sound(self):
+        #self.sound = AudioSegment.from_wav("/home/pi/Documents/Projects/pi-visit/sounds/beep-sound.wav")
+
+        pygame.init()
+        pygame.mixer.music.load("/home/pi/Documents/Projects/pi-visit/sounds/beep-sound.wav")
+
+        #self.sound = pygame.mixer.Sound("")
+
+    def play_success_sound(self):
+
+        print('play sound')
+
+        pygame.mixer.music.play()
+        
+        #play(self.sound)
+
+        #playsound("/home/pi/Documents/Projects/pi-visit/sounds/beep-sound.mp3")
+        
+        
 
         
 
